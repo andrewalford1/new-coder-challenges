cb50f591cb75a287456174f65c0ae1db536f0f84
Andrew Alford <Andrew.Alford@tracerco.com>
2023-07-12 11:19:26 +0100

    add the fibbonacci sequence challange

--- 02-FibonacciCalculator/Program.cs
+++ 02-FibonacciCalculator/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your number:");
+
+string userInput = Console.ReadLine();
+int sequenceNumber = Int32.Parse(userInput);
+
+int previousValue = 0;
+int nextValue = 1;
+
+for (int i = 1; i < sequenceNumber; i++)
+{
+    int sum = previousValue + nextValue;
+    previousValue = nextValue;
+    nextValue = sum;
+    Console.WriteLine(nextValue);
+}

8ea6a857c1f691737656c931fc5c4e22c481f028
Andrew Alford <Andrew.Alford@tracerco.com>
2023-07-12 10:48:45 +0100

    add the solution for the guess the number challange

--- 01-GuessTheNumber/Program.cs
+++ 01-GuessTheNumber/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Think of a number between 1 and 100");
+
+var random = new Random();
+
+bool isCorrectGuess = false;
+
+int min = 1;
+int max = 101;
+
+while (isCorrectGuess == false)
+{
+    int guess = random.Next(min, max);
+
+    Console.WriteLine($"Is your number {guess}? [Y/N]");
+
+    string userInput = Console.ReadLine().ToUpper();
+
+    if (userInput == "Y")
+    {
+        isCorrectGuess = true;
+        continue;
+    }
+
+    Console.WriteLine($"Is your number greater than or less than {guess}? [G/L]");
+
+    userInput = Console.ReadLine().ToUpper();
+
+    if (userInput == "G")
+    {
+        min = guess + 1;
+    }
+    else if (userInput == "L")
+    {
+        max = guess;
+    }
+}
